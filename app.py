cebdf4378a0d619c1cec09af2d277d354c597020
yuvalsi3 <yuval.sisso22@gmail.com>
2025-05-17 15:30:51 +0300

    s

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def get_weather_for_city(city):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
